7b518ff37c7cd66f230d17f4bd752949d269f7eb
QuiasZ <138547828+QuiasZ@users.noreply.github.com>
2024-09-24 12:08:10 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,32 @@
+// gulpfile.js
+
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
+
+async function images() {
+    const imagemin = await import('gulp-imagemin');
+    return gulp.src('./src/images/*', { encoding: false })
+        .pipe(imagemin.default())
+        .pipe(gulp.dest('./dist/images'));
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({
             outputStyle: 'compressed'
         }))
-        .pipe(gulp.dest('./dist/css'))
+        .pipe(gulp.dest('./dist/css'));
 }
 
-function images() {
-    return gulp.src('./src/images/*', { encoding: false })
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'))
-}
 function scripts() {
     return gulp.src('./src/scripts/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('./dist/js'))
+        .pipe(gulp.dest('./dist/js'));
 }
 
 exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
-}
+};
